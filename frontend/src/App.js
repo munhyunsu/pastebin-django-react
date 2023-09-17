261d137fb61559419f84a952e956286796bd57e5
munhyunsu <munhyunsu@gmail.com>
2023-09-18 03:14:55 +0900

    Timeline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const PASTES = [
+  {"id": 1, "user": "user1", "utime": "2023-09-17T14:04:04.886488Z", "content": "user1 content"},
+  {"id": 2, "user": "user2", "utime": "2023-09-17T14:04:12.778037Z", "content": "user2 content"},
+  {"id": 3, "user": "user2", "utime": "2023-09-17T14:06:34.077729Z", "content": "user2 content"}
+];
+
+function WriteBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form action="index.html" method="post">
+      <label for="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter username" name="username" required></input>
+      <label for="content"><b>Content</b></label>
+      <textarea name="content" placeholder="Enter content" required></textarea>
+      <button type="submit">Paste Note</button>
+    </form>
+  );
+}
+
+function Card ({ paste }) {
+  return (
+    <div>
+      <h3>{paste.user}</h3>
+      <p>{paste.content}</p>
     </div>
   );
+}
+
+function Timeline ({ pastes }) {
+  const rows = [];
+
+  pastes.forEach((paste) => {
+    rows.push(<Card paste={paste} />);
+  });
+
+  return (
+    <div>
+      {rows}
+    </div>
+  );
+}
+
+function WritableTimeline({ pastes }) {
+  return (
+    <div>
+      <WriteBar />
+      <Timeline pastes={pastes} />
+    </div>
+  );
+}
+
+function App() {
+  return <WritableTimeline pastes={PASTES} />;
 }
 
 export default App;
