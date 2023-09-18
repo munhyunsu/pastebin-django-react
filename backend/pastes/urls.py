9b852ac53a663d5b7c02077f5687074e65e964c3
munhyunsu <munhyunsu@gmail.com>
2023-09-18 12:58:59 +0900

    prefix

--- backend/pastes/urls.py
+++ backend/pastes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from pastes import views
 
 urlpatterns = [
-    path('api/v1/pastes/', views.paste_list),
-    path('api/v1/pastes/<int:pk>/', views.paste_detail),
+    path('pastebin/api/v1/pastes/', views.paste_list),
+    path('pastebin/api/v1/pastes/<int:pk>/', views.paste_detail),
 ]
